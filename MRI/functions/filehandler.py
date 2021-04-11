9bb72e2bc6b1d45630d47ab9ad27d53c1f39714f
Robin Mattias Nilsson <robinmattias@hotmail.com>
2021-04-11 09:21:26 +0200

    Images are view correctly now

--- MRI/functions/filehandler.py
+++ MRI/functions/filehandler.py
@@ -12,6 +12,17 @@ def save_nifti_image(complex_im, filename, meta_data):
         print("Unexpected error:", sys.exc_info()[0])
     else:
         print(f"File {filename} saved succesfully!")
+
+        
+def k2im(k_space):
+    img = np.empty(k_space.shape, dtype='float').astype(complex)
+    
+    for i in range((k_space.shape)[2]):
+        img[:,:,i] = np.fft.ifftshift(np.fft.ifft2(k_space[:,:,i]))
+
+    # img = np.fft.ifftshift(np.fft.ifftn(k_space))
+    
+    return img
         
 def test_func():
     print("some stuff")
