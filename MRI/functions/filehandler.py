82f8af57393fc5b0439d636843742cd64dd1e964
Robin Nilsson <robinmattias@hotmail.com>
2021-04-23 11:37:34 +0200

    solved SENSE

--- MRI/functions/filehandler.py
+++ MRI/functions/filehandler.py
@@ -32,27 +32,29 @@ def k2im(k_space):
 def SENSE(folded_ims, coil_sensitivities):
     
     n = coil_sensitivities.shape
-    Ny = int(n[0] / 2)
-    
-    outputImage = np.empty(n[0:3], dtype='float')
+    Ny2 = int(n[0] / 2)
+    Ny4 = int(Ny2 / 2)
+    outputImages = np.empty(n[0:3], dtype='float')
     
     for img in range(n[2]): # for each image
-        for x in range(n[1]): # left - right
-            for y in range(Ny): # top - bottom
-                C = folded_ims[y, x, img, :]
-                S = coil_sensitivities[[y, (y + Ny) % (n[0] - 1)], x, img, :]
+        
+        for y in range(Ny2): # top - bottom of folded images
+            ys = [y + Ny4, (y + Ny4 + Ny2) % n[0]]
+            
+            for x in range(n[1]): # left - right
+                f = folded_ims[y, x, img, :]
+                S = np.transpose(coil_sensitivities[ys, x, img, :])
                 
+                sol,_,_,_ = lin.lstsq(S,f, rcond=-1)
                 
-                sol,_,_,_ = lin.lstsq(S,C, rcond=-1)
-                
-                outputImage[[y, (y + Ny) % (n[0] - 1)], x, img] = sol 
+                outputImages[ys, x, img] = sol 
     
-    return outputImage
+    return outputImages
 
 '''
-S1(x,y) = C1(x,y)I(x,y) + C2(x,y+ny/2)I(x,y+ny/2)
+f1(x,y) = S1(x,y)I(x,y) + S1(x,y+ny/2)I(x,y+ny/2)
 And
-S2(x,y) = C1(x,y)I(x,y) + C1(x,y+ny/2)I(x,y+ny/2)
+f2(x,y) = S2(x,y)I(x,y) + S2(x,y+ny/2)I(x,y+ny/2)
 for each pixels.
-Where S1-2 is the signal images (folded) and C1-2 is the coil images and I is the image we want reconstruct. 
+Where f1-2 is the folded images and S1-2 is the coil sensitivity images and I is the image we want reconstruct. 
 '''
