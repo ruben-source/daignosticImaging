33eef9d0947a56472027fd2b3a23c0e4c10cd187
Robin Mattias Nilsson <robinmattias@hotmail.com>
2021-05-24 16:36:28 +0200

    SOme changes

--- PET/Functions/compute_forward_matrix.m
+++ PET/Functions/compute_forward_matrix.m
@@ -1,4 +1,10 @@
 function A = compute_forward_matrix(thetas, L, R, C)
+    % G = radon(F) 
+    %   = A * F   {what we want find}
+    %   = A * (I * F)
+    %   = (A * I) * F
+    %   = radon(I) * F => A = radon(I)
+
     % rL = size(radon(zeros(R, C), thetas(1)), 1);
     A = zeros(L*length(thetas), R*C);
     
@@ -7,7 +13,8 @@ function A = compute_forward_matrix(thetas, L, R, C)
         for j = 1:R*C
             I = zeros(R, C); I(j) = 1; % only one base
             rad = radon(I, thetas(i));
-            A(idx, j) = rad(1:L);
+            A(idx,j) = rad(floor((end/2-L/2+1):(end/2+L/2)));
+            % error("stop")
         end
     end
 
